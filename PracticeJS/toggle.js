51704faacf6eab403487f40f32f029ff9bf1f69e
shji023 <shji0@naver.com>
2021-04-19 17:36:22 +0900

    modal

--- PracticeJS/toggle.js
+++ PracticeJS/toggle.js
@@ -2,3 +2,21 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
