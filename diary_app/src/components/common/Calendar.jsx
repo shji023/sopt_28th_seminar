a5e29a001d09a2f425b40c7c44fa2dc3eb07bbfe
JiyoungHyun <shyunji023@gmail.com>
2021-06-08 16:02:06 +0900

    feat:favicon설정, Calendar 년, 월 이동 수정

--- diary_app/src/components/common/Calendar.jsx
+++ diary_app/src/components/common/Calendar.jsx
@@ -1,9 +1,10 @@
-import React, {useRef} from 'react';
-import LeftOn from '../../assets/LeftOn.svg'
-import LeftOff from '../../assets/LeftOff.svg'
-import RightOn from '../../assets/RightOn.svg'
-import RightOff from '../../assets/RightOff.svg'
-import Styled from 'styled-components';
+import React, { useRef } from "react";
+import LeftOn from "../../assets/LeftOn.svg";
+import LeftOff from "../../assets/LeftOff.svg";
+import RightOn from "../../assets/RightOn.svg";
+import RightOff from "../../assets/RightOff.svg";
+import Styled from "styled-components";
+import { withRouter } from "react-router-dom";
 
 const CalendarWrap = Styled.div`
   .calendar {
@@ -50,56 +51,79 @@ const CalendarWrap = Styled.div`
   }
 `;
 
+const Calendar = ({
+  currYear,
+  setCurrYear,
+  currMonth,
+  setCurrMonth,
+  history,
+  location,
+}) => {
+  const isMain = location.pathname === "/" ? true : false;
+  const monthList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  const leftButton = useRef();
+  const rightButton = useRef();
 
-const Calendar = (props) => {
-    const {currYear, setCurrYear, currMonth, setCurrMonth} = props;
-    const monthList =[0,1,2,3,4,5,6,7,8,9,10,11];
-    const leftButton = useRef();
-    const rightButton = useRef();
-    
-    return (
-      <CalendarWrap>
-        <div className="calendar">
-          <div className="calendar__year">
-            <img 
-              className="calendar__year--left" 
-              src={LeftOff} 
-              alt=""
-              onClick={()=>{setCurrYear(currYear -1)}}
-              onMouseEnter={()=>{leftButton.current.src=LeftOn}}
-              onMouseLeave={()=>{leftButton.current.src=LeftOff}} 
-              ref={leftButton}
-            />
-            <div className="calendar__year--number">{currYear}년</div>
-            <img 
-              className="calendar__year--right" 
-              src={RightOff} 
-              alt=""
-              onClick={()=>{setCurrYear(currYear + 1)}}
-              onMouseEnter={()=>{rightButton.current.src=RightOn}}
-              onMouseLeave={()=>{rightButton.current.src=RightOff}}
-              ref={rightButton}
-            />
-          </div>
-          <div className="calendar__month">
-          {monthList.map((month)=>{
-                return (
-                <div 
-                  key={month} 
-                  className="calendar__month--button" 
-                  onClick={()=>setCurrMonth(month)}
-                  style={month===currMonth?{fontSize:"22px", fontWeight:"bold"}:{}}
-                > 
-                    {month + 1}월
-                </div>
-                );
-            }
-          )}
-          </div> 
+  return (
+    <CalendarWrap>
+      <div className="calendar">
+        <div className="calendar__year">
+          <img
+            className="calendar__year--left"
+            src={LeftOff}
+            alt=""
+            onClick={() => {
+              isMain && setCurrYear(currYear - 1);
+            }}
+            onMouseEnter={() => {
+              leftButton.current.src = LeftOn;
+            }}
+            onMouseLeave={() => {
+              leftButton.current.src = LeftOff;
+            }}
+            ref={leftButton}
+          />
+          <div className="calendar__year--number">{currYear}년</div>
+          <img
+            className="calendar__year--right"
+            src={RightOff}
+            alt=""
+            onClick={() => {
+              isMain && setCurrYear(currYear + 1);
+            }}
+            onMouseEnter={() => {
+              rightButton.current.src = RightOn;
+            }}
+            onMouseLeave={() => {
+              rightButton.current.src = RightOff;
+            }}
+            ref={rightButton}
+          />
         </div>
-      </CalendarWrap>
-        
-    );
+        <div className="calendar__month">
+          {monthList.map((month) => {
+            return (
+              <div
+                key={month}
+                className="calendar__month--button"
+                onClick={() => {
+                  setCurrMonth(month);
+                  history.push(`/`);
+                }}
+                style={
+                  month === currMonth
+                    ? { fontSize: "22px", fontWeight: "bold" }
+                    : {}
+                }
+              >
+                {month + 1}월
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </CalendarWrap>
+  );
 };
 
-export default Calendar;
+export default withRouter(Calendar);
